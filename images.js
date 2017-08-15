e6ec85ff691c43c5a48effd10fc3dd1364aa943f
Leox Fernandez <leox.fernandez@gmail.com>
2017-08-15 09:10:03 +0800

    minor changes

--- images.js
+++ images.js
@@ -22,7 +22,14 @@ $( document ).on( "pagecreate", "#images", function () {
 
       $weddings = $("#weddingsImages"),
       $weddingsImages = $weddings.find(".imageBox"),
-      $slidingweddings = $( "#weddingsImages .sliderBox" ).find("img");
+      $slidingweddings = $( "#weddingsImages .sliderBox" ).find("img"),
+
+      roomsImagesVisible = false,
+      restaurantsVisible = false,
+      landscapesVisible = false,
+      activitiesVisible = false,
+      mogambospringsVisible = false,
+      weddingsVisible = false;
 
   var cycleNames = [
     {"name": $rooms, "modalDiv": '#open-rooms', "Images": $roomsImages, "urls": roomsImgUrls}, 
@@ -32,48 +39,6 @@ $( document ).on( "pagecreate", "#images", function () {
     {"name": $mogambosprings, "modalDiv": '#open-mogambosprings', "Images": $mogambospringsImages, "urls": mogambospringsImgUrls },
     {"name": $weddings, "modalDiv": '#open-weddings', "Images": $weddingsImages, "urls": weddingsImgUrls }
   ]
-
-  for ( var cycleCnt = cycleNames.length - 1; cycleCnt >= 0; cycleCnt-- ) {
-    $(cycleNames[cycleCnt].Images).cycle({
-      autoHeightSpeed: 1000,
-      speed: 1000,
-      timeout: 10000,
-      manualSpeed: 1000,
-      fx: "fade",
-      swipe: true,
-      swipefx: "fade",
-      centerHorz: true,
-      centerVert: true,
-      pause: true,
-      allowWrap: false,
-      loader: true,
-      progressive: cycleNames[cycleCnt].urls,
-      log: false
-    });
-
-    $(cycleNames[cycleCnt].Images).on("cycle-before", function(e, options, outgoing, incoming) {
-        updatePosition();
-    });
-
-    doCycleAfter(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
-
-    $(cycleNames[cycleCnt].name).apFullscreenModal({
-      openSelector: cycleNames[cycleCnt].modalDiv,
-      animationDuration: 800
-    });
-
-    $(cycleNames[cycleCnt].name).find(".btnCaption").on("click", function() {
-        $(".slideCaption").toggle();
-    })
-
-    gotoNext(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
-    gotoPrev(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
-    doPlayPause(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
-
-    $(cycleNames[cycleCnt].modalDiv).on("click", function() {
-      console.log("hi");
-    });
-  }
 
   function gotoNext(name,images) {
     $(name).find(".btnNext").on("click", function() {
@@ -100,36 +65,127 @@ $( document ).on( "pagecreate", "#images", function () {
     });
   }
 
+  function doCycleBefore(name,images){
+    $(images).on("cycle-before", function(e, options, outgoing, incoming) {
+        console.log("Updating position...cycle-before", images.selector);
+        updatePosition(images);
+    });
+  }
+
   function doCycleAfter(name,images){
     $(images).on("cycle-after", function(e, options, outgoing, incoming) {
+      // console.log("Updating position...cycle-after", images.selector);
+      // updatePosition(images);
       $(name).find("h2").html($(incoming).data("name"));
       $(name).find("p").html($(incoming).data("description"));
     });
   }
 
-  function updatePosition() {
+  function doCycleResume(modalDiv,images){
+    $(modalDiv).on("click", function() {
+      console.log("Pause all slide shows");
+      pauseSlideShows();
+      console.log("starting slideshow", images.selector);
+      $(images).cycle('goto', 0);
+      updatePosition(images);
+      $(images).cycle("resume");
+      return false;
+    });
+  }
+
+  function updatePosition(pSlideName) {
     var theHeight = $( window ).height();
+    $(pSlideName).css("maxHeight",theHeight);
+    $(pSlideName).find("img").css("maxHeight",theHeight);  
+  }
+
+  function pauseSlideShows() {
+    //Temporary solution to stop running slideshow, find a way to detect
+    //currently running slideshow and only pause that specifific slideshow
     for ( var cycleCnt = cycleNames.length - 1; cycleCnt >= 0; cycleCnt-- ) {
-      $(cycleNames[cycleCnt].Images).css("maxHeight",theHeight);
-      $(cycleNames[cycleCnt].Images).find("img").css("maxHeight",theHeight);
-    }
+      $(cycleNames[cycleCnt].Images).cycle("pause");;  
+    };
+  }
+
+  function startAllSlideShows() {
+    //Temporary solution to stop running slideshow, find a way to detect
+    //currently running slideshow and only pause that specifific slideshow
+    for ( var cycleCnt = cycleNames.length - 1; cycleCnt >= 0; cycleCnt-- ) {
+      $(cycleNames[cycleCnt].Images).cycle("resume");;  
+    };
+  }
+
+  for ( var cycleCnt = cycleNames.length - 1; cycleCnt >= 0; cycleCnt-- ) {
+    $(cycleNames[cycleCnt].Images).cycle({
+      // pause: true,
+      autoHeightSpeed: 1000,
+      speed: 1000,
+      timeout: 10000,
+      manualSpeed: 1000,
+      fx: "fade",
+      swipe: true,
+      swipefx: "fade",
+      centerHorz: true,
+      centerVert: true,
+      pause: true,
+      allowWrap: false,
+      // loader: true,
+      progressive: cycleNames[cycleCnt].urls,
+      log: false
+    });
+
+    $(cycleNames[cycleCnt].name).apFullscreenModal({
+      openSelector: cycleNames[cycleCnt].modalDiv,
+      closeSelector: '.close-modal',
+      animationDuration: 800
+    });
+
+    // $(cycleNames[cycleCnt].Images).cycle("pause");
+
+    doCycleAfter(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
+    doCycleBefore(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
+    gotoNext(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
+    gotoPrev(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
+    doPlayPause(cycleNames[cycleCnt].name,cycleNames[cycleCnt].Images);
+    doCycleResume(cycleNames[cycleCnt].modalDiv,cycleNames[cycleCnt].Images);
+
+    $(cycleNames[cycleCnt].name).find(".btnCaption").on("click", function() {
+        $(".slideCaption").toggle();
+    })
   }
 
   $( window ).resize( function (event) {
     event.preventDefault();
-    updatePosition();
+    // console.log("Updating position...resize");
+    for ( var cycleCnt = cycleNames.length - 1; cycleCnt >= 0; cycleCnt-- ) {
+      updatePosition(cycleNames[cycleCnt].Images);
+    };
   });
-
-  updatePosition();
 
 
   $(".slideCaption").toggle();
 
+  $(".apfm-close-button").on("click", function() {
+    startAllSlideShows();
+  });
 
-  // $rooms.apFullscreenModal({
-  //   openSelector: '#open-rooms',
-  //   // backgroundColor: '#000',
-  // });
+  $(document.documentElement).keyup(function (e) {
+
+      if (e.keyCode == 39)
+      {        
+         // $roomsImages.cycle('next');
+      }
+
+      if (e.keyCode == 37)
+      {
+          // $roomsImages.cycle('prev');
+      }
+
+      if (e.keyCode == 27){
+          // Temp. solution to continue download of slideshows pics while not shown.
+          startAllSlideShows();
+      }
+  });
   
   // $roomsImages.cycle({
   //   autoHeightSpeed: 100,
